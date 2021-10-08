1b2212805ccee7b540d2a5af8ef194d6a34da334
Kevin Cole <kfcole20@gmail.com>
2021-10-08 16:22:03 -0500

    Added recipe and ingredient models

--- main_app/forms.py
+++ main_app/forms.py
@@ -9,4 +9,9 @@ class Registration_Form(forms.Form):
     last_name= forms.CharField(label='Last Name' , max_length=20, min_length=2)
     email= forms.EmailField(label='Email', max_length=20, min_length=6)
     password= forms.CharField(label='Password', widget=forms.PasswordInput(), min_length=8)
-    confirm_password= forms.CharField(label='Confirm Password', widget=forms.PasswordInput(), min_length=8)       
+    confirm_password= forms.CharField(label='Confirm Password', widget=forms.PasswordInput(), min_length=8) 
+
+class Recipe_Form(forms.Form):
+    name= forms.CharField(label='Recipe Name', max_length=40, min_length=2)
+    description=forms.TextInput()
+    
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import TextField
+from django.db.models.fields import related
+from django.db.models.fields.related import ManyToManyField
+from main_app.forms import Recipe_Form
+from users_app.models import User
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name= models.CharField(max_length=255)
+    description= models.TextField()
+    uploaded_by=models.ForeignKey(User, related_name='recipes', on_delete=CASCADE)
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+class Ingredient(models.Model):
+    name= models.CharField(max_length=255)
+    recipe_for= models.ManyToManyField(Recipe, related_name='ingredients_needed')
+    created_at= models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
